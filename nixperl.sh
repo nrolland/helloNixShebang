9c4027c4946d43d79ab2305e95613521aa51f5ba
nicolas rolland <nicolas.roland@gmail.com>
2018-11-28 17:58:19 +0100

    perl and python from doc

---
+++ nixperl.sh
@@ -0,0 +1,14 @@
+#! /usr/bin/env nix-shell
+#! nix-shell -i perl -p perl perlPackages.HTMLTokeParserSimple perlPackages.LWP
+
+use HTML::TokeParser::Simple;
+
+# Fetch nixos.org and print all hrefs.
+my $p = HTML::TokeParser::Simple->new(url => 'http://nixos.org/');
+
+while (my $token = $p->get_tag("a")) {
+    my $href = $token->get_attr("href");
+    print "$href\n" if $href;
+}
+
+
